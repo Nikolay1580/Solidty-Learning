dedd30c14530f7426aaacb21dd0df7bc1478d257
NikolayLachezarov <55876629+NikolayLachezarov@users.noreply.github.com>
2021-10-31 14:34:54 +0200

    Event listener to print out WASD input

--- Website/logic.js
+++ Website/logic.js
@@ -3,7 +3,22 @@ let box = document.getElementById('box');
 
 var c = document.getElementById("Canvas");
 
-
+window.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case "w":
+            console.log('w');
+            break;
+        case "a":
+            console.log('a');
+            break;
+        case "s":
+            console.log('s');
+            break;
+        case "d":
+            console.log('d');
+            break;
+    }
+});
 
 
 
